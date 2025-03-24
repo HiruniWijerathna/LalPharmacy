b0ea68719e4db0e8cbe42688fd62b7bd1e4f7bb4
Hiruni Wijerathna <114440722+HiruniWijerathna@users.noreply.github.com>
2025-03-24 23:13:01 +0530

    add

--- Frontend/src/Components/Footer/Footer.jsx
+++ Frontend/src/Components/Footer/Footer.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import './Footer.css'
+import fb_icon from '../../assets/fb.png';
 
 const Footer = () => {
   return (
     <div className='footer'>
       <p>© 2025 LAL Pharmacy - All Rights Reserved</p>
-      <ul>
-      <li>Home</li>
-        <li>Service</li>
-        <li>About Us</li>
-        <li>Contact Us</li>
-      </ul>
+        <div className='box1'>
+         <img src={fb_icon} alt="Facebook" />
+            <div className='fblist'>
+             <ul>
+              <li><a href="https://www.facebook.com/yourpage" target="_blank" rel="noopener noreferrer">
+                   Follow us on Facebook</a>
+              </li>
+             </ul>
+            </div>
+        </div>
     </div>
   )
 }
